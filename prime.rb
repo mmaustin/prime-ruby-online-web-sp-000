7e6d841a49e82798508dadd7280f458a74c68144
mccray austin <tubach75@hotmail.com>
2020-09-24 22:56:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
-# Add  code here!
+=begin# Add  code here!
 def prime?(num)
   if num < 2
     return false
@@ -13,3 +13,26 @@ def prime?(num)
 end
 
 prime?(11)
+=end
+
+
+def prime?(num)
+  return true if num < 2
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
+prime?(7)
+
+
+
+
+
+
+
+
+
+
+

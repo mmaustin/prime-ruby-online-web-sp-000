720129902a38a9c79f6e4178883a7807c630b658
mccray austin <tubach75@hotmail.com>
2020-09-24 22:59:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
-# Add  code here!
+=begin
+def prime?(num)
+  if num < 2
+    return false
+  elsif
+  (2..(num - 1)).each do |n|
+     num % n == 0
+      return false
+    end
+  else
+     true
+    end
+end
+
+prime?(11)
+=end
+
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
+prime?(7)

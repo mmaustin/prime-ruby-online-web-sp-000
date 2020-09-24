8893f008c45d30d0fe15eb5aed850e48138c96be
mccray austin <tubach75@hotmail.com>
2020-09-24 22:42:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 # Add  code here!
 def prime?(num)
+  if num < 2
+    false
+  elsif
   (2..(num - 1)).each do |n|
     return if num % n == 0
       false
     end
+  else
       true
+    end
 end
 
 prime?(11)
